9e452c403325c588436da49055201df0ccd87bb8
smoothwarhead <simeont126@outlook.com>
2022-08-28 12:37:21 -0500

    latest commit

--- config/corsMiddleware.js
+++ config/corsMiddleware.js
@@ -2,7 +2,8 @@ const cors = require('cors');
 
 const corsOptions = {
     origin: "https://immediate.netlify.app",
-    methods: ["GET", "POST", "DELETE", "PUT"],
+    methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Origin", "X-Requested"],
     credentials: true
 }
 
