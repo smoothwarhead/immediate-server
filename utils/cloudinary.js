a0a0d759f7774c9bfb57b59f82a6b613336512c8
smoothwarhead <simeont126@outlook.com>
2022-09-10 16:29:03 -0500

    latest commit

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -3,9 +3,14 @@ const cloudinary = require('cloudinary').v2;
 
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: "greenietec",
+    api_key: "184763843515448",
+    api_secret: "npKY_g1ejcUeP3SW2xK7qZHRBxI",
+
+
+    // cloud_name: process.env.CLOUDINARY_NAME,
+    // api_key: process.env.CLOUDINARY_API_KEY,
+    // api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 
